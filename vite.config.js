b4386f64c9025bf9eb1793a8372aa60cf639237b
Lakshmi Karthika <lakshmikarthika07@gmail.com>
2025-10-08 12:57:02 +0530

    change in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'ui-vendor': ['lucide-react'],
+          'axios': ['axios'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+  }
 })
