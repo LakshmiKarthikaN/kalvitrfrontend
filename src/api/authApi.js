1e3bf49aa0f03faa2a6871e605d9ac6009babf9c
Lakshmi Karthika <lakshmikarthika07@gmail.com>
2025-10-09 20:24:38 +0530

    change api path

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
-
-let apiUrl = import.meta.env.VITE_API_URL;
-console.log("API Base URL:", apiUrl);
+let apiUrl;
+try {
+  apiUrl = import.meta.env?.VITE_API_URL || "https://www.kalvi-track.co.in/";
+} catch (error) {
+  apiUrl = "https://www.kalvi-track.co.in/";
+}
 
 export const api = axios.create({
   baseURL: apiUrl,
