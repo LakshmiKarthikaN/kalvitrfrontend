5fd0925e5d57da52d1cf58faa81aafe8f8812a0c
Lakshmi Karthika <lakshmikarthika07@gmail.com>
2025-10-09 12:58:57 +0530

    edit https

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -3,9 +3,9 @@ import axios from "axios";
 
 let apiUrl;
 try {
-  apiUrl = import.meta.env?.VITE_API_URL || "http://kalvitrackweb-env.eba-f54ugkwp.eu-north-1.elasticbeanstalk.com/api";
+  apiUrl = import.meta.env?.VITE_API_URL || "https://kalvitrackweb-env.eba-f54ugkwp.eu-north-1.elasticbeanstalk.com";
 } catch (error) {
-  apiUrl = "http://kalvitrackweb-env.eba-f54ugkwp.eu-north-1.elasticbeanstalk.com/api"; // Changed to http
+  apiUrl = "https://kalvitrackweb-env.eba-f54ugkwp.eu-north-1.elasticbeanstalk.com"; // Changed to http
 }
 
 export const api = axios.create({
